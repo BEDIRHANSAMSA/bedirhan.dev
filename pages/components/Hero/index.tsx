aa7eaf1d3c9bcd286666678c6fbf69d2ad806e86
Bedirhan SAMSA <me@bedirhan.dev>
2022-03-14 19:22:40 +0300

    converted espace character

--- pages/components/Hero/index.tsx
+++ pages/components/Hero/index.tsx
@@ -9,8 +9,8 @@ export default function Hero() {
           Full Stack Developer
         </Heading>
         <Text fontSize={20} mt={5} maxW="xl">
-          I'm creating modern and responsive applications and interested in
-          topics such as C#, Computer Technologies, science, sociology. I'm
+          I{"'"}m creating modern and responsive applications and interested in
+          topics such as C#, Computer Technologies, science, sociology. I{"'"}m
           2nd-year student at Istanbul Medipol University.
         </Text>
       </Flex>
